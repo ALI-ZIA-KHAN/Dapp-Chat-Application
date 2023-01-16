3be8911952764ca00f2985189c68fad6770c63a3
NimrahAltafAdam <nimrahadam.123@gmail.com>
2023-01-16 10:00:24 +0500

    UI Changes

--- pages/about.js
+++ pages/about.js
@@ -7,31 +7,31 @@ import React from 'react'
       <div className='row justify-content-center'>
           <div className='col-md-6 col-sm-12 col-lg-4 d-flex justify-content-center'>
           <div class="card" style={{width:"18rem"}}>
-  <img src="https://elearningindustry.com/wp-content/uploads/2017/12/fbe24f70860807bdea49bc435e8b70bd.png" class="card-img-top" alt="..." />
+  <img src="https://cdn-icons-png.flaticon.com/512/2504/2504165.png" class="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title">Mobile Friendly</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Read More</a>
   </div>
 </div>
           </div>
           <div className='col-md-6 col-sm-12 col-lg-4 d-flex justify-content-center'>
           <div className="card" style={{width: "18rem"}}>
-<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxxqeTKYUzG7WVmroD4M3CQFRKDW30yPnejA&usqp=CAU' className="card-img-top"/>
+<img src='https://cdn-icons-png.flaticon.com/512/6553/6553508.png' className="card-img-top"/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title">Fast Connection</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Read More</a>
   </div>
 </div>
           </div>
           <div className='col-md-12 col-sm-12 col-lg-4 d-flex justify-content-center'>
           <div className="card" style={{width: "18rem"}}>
-  <img src="https://static9.depositphotos.com/1144687/1117/i/600/depositphotos_11176245-stock-photo-security-identification.jpg" className="card-img-top" alt="..." />
+  <img src="https://cdn-icons-png.flaticon.com/512/6230/6230874.png" className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title">Language Friendly</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">Read More</a>
   </div>
 </div>
           </div>
